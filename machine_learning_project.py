3a5af63a322ba8ba3f5a418ab7e58ba4342ebe05
thishanthishan <144465245+thishanthishan@users.noreply.github.com>
2026-05-18 21:00:36 +0530

    Update machine_learning_project.py

--- machine_learning_project.py
+++ machine_learning_project.py
@@ -1,65 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-
-# Load dataset
 df = pd.read_csv("raw_dataset.csv")
-
-# Remove duplicates
 df.drop_duplicates(inplace=True)
-
-# Fill missing values
 df['Age'] = df['Age'].fillna(df['Age'].mean())
 df['Salary'] = df['Salary'].fillna(df['Salary'].mean())
-
-# Create Salary Category
 df['Salary_Category'] = df['Salary'].apply(
     lambda x: 'High' if x > 70000 else 'Low'
 )
-
-# Encode Department column
 encoder = LabelEncoder()
 df['Department'] = encoder.fit_transform(df['Department'])
-
-# Features and Target
 X = df[['Age', 'Department', 'Experience_Years', 'Performance_Score']]
 y = df['Salary_Category']
-
-# Split dataset
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-
-# Train model
 model = DecisionTreeClassifier()
-
 model.fit(X_train, y_train)
-
-# Predictions
 y_pred = model.predict(X_test)
-
-# Accuracy
 accuracy = accuracy_score(y_test, y_pred)
-
 print("Accuracy:", accuracy)
-
-# Classification report
 print("\nClassification Report:\n")
 print(classification_report(y_test, y_pred))
-
-# Confusion Matrix
 cm = confusion_matrix(y_test, y_pred)
-
 plt.figure(figsize=(6,4))
 sns.heatmap(cm, annot=True, fmt='d')
-
 plt.title("Confusion Matrix")
 plt.xlabel("Predicted")
 plt.ylabel("Actual")
-
 plt.show()
